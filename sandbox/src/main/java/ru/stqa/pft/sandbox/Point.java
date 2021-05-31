d9852db13d6d4e289d63726d1119aaddb482c060
el-zat <elena.zatykina@gmail.com>
2021-05-31 16:51:08 +0200

    Task2 added

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+public class Pointp1 {
+
+    public double xa;
+    public double ya;
+
+    public Pointp1 (double xa, double ya) {
+        this.xa = xa;
+        this.ya = ya;
+    }
+
+
+//        public double distance(){
+//            return Math.sqrt(Math.pow((this.xb - this.xa), 2) + Math.pow((this.yb-this.ya), 2));
+//        }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(3, 2);
+        Point p2 = new Point(6, 4);
+
+        System.out.println("Distance between two points is "+ p1.distance(p2)+p2.distance(p1));
+    }
+
+    }
+
