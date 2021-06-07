338a1212f3ec4ecb476f36e0b932c6f442f696c0
el-zat <elena.zatykina@gmail.com>
2021-06-07 09:36:42 +0200

    replace inheritance with deligation

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/ApplicationManager.java
@@ -0,0 +1,92 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private WebDriver wd;
+
+    public void init() {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/addressbook/");
+        login("admin", "secret");
+    }
+
+    public void login(String username, String password) {
+      wd.findElement(By.name("user")).click();
+      wd.findElement(By.name("user")).clear();
+      wd.findElement(By.name("user")).sendKeys(username);
+      wd.findElement(By.name("pass")).click();
+      wd.findElement(By.name("pass")).clear();
+      wd.findElement(By.name("pass")).sendKeys(password);
+      wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    public void logout() {
+      wd.findElement(By.linkText("Logout")).click();
+    }
+
+    public void returnToGroupPage() {
+      wd.findElement(By.linkText("group page")).click();
+    }
+
+    public void submitGroupCreation() {
+      wd.findElement(By.name("submit")).click();
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+      wd.findElement(By.name("group_name")).click();
+      wd.findElement(By.name("group_name")).clear();
+      wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+      wd.findElement(By.name("group_header")).click();
+      wd.findElement(By.name("group_header")).clear();
+      wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+      wd.findElement(By.name("group_footer")).click();
+      wd.findElement(By.name("group_footer")).clear();
+      wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    public void initGroupCreation() {
+      wd.findElement(By.name("new")).click();
+    }
+
+    public void gotoGroupPage() {
+      wd.findElement(By.linkText("groups")).click();
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+    public boolean isElementPresent(By by) {
+      try {
+        wd.findElement(by);
+        return true;
+      } catch (NoSuchElementException e) {
+        return false;
+      }
+    }
+
+    public boolean isAlertPresent() {
+      try {
+        wd.switchTo().alert();
+        return true;
+      } catch (NoAlertPresentException e) {
+        return false;
+      }
+    }
+
+    public void deleteSelectedGroups() {
+      wd.findElement(By.name("delete")).click();
+    }
+
+    public void selectGroup() {
+      wd.findElement(By.name("selected[]")).click();
+    }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+
+    protected final ApplicationManager app = new ApplicationManager();
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown()  {
+        app.stop();
+    }
+
+}
