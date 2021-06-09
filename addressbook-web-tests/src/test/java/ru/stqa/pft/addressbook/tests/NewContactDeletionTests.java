7be760c71ad38606398234ffef6d0a865d6e3fda
el-zat <elena.zatykina@gmail.com>
2021-06-09 14:24:53 +0200

    NewContactDeletion created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactDeletionTests.java
@@ -1,2 +1,17 @@
-package ru.stqa.pft.addressbook.tests;public class NewContactDeletionTests {
+
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+
+public class NewContactDeletionTests extends TestBase {
+
+  @Test
+  public void newContactDeletion() {
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().selectContact();
+    app.getContactHelper().contactDeletion();
+    app.getContactHelper().alert();
+
+  }
 }
