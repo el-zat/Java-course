0361f972f04d93d1a499dc55e7f0ec0c29cdc86d
el-zat <elena.zatykina@gmail.com>
2021-06-09 14:30:42 +0200

    application manager corrected

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,93 +1,44 @@
-package ru.stqa.pft.addressbook.appmanager.tests;
+package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
-import ru.stqa.pft.addressbook.model.GroupData;
-
 import java.util.concurrent.TimeUnit;
 
+
 public class ApplicationManager {
-    private WebDriver wd;
+  ChromeDriver wd;
 
-    public void init() {
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/addressbook/");
-        login("admin", "secret");
-    }
+  private SessionHelper sessionhelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+  private ContactHelper contactHelper;
 
-    public void login(String username, String password) {
-      wd.findElement(By.name("user")).click();
-      wd.findElement(By.name("user")).clear();
-      wd.findElement(By.name("user")).sendKeys(username);
-      wd.findElement(By.name("pass")).click();
-      wd.findElement(By.name("pass")).clear();
-      wd.findElement(By.name("pass")).sendKeys(password);
-      wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
+  public void init() {
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/addressbook/");
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionhelper = new SessionHelper(wd);
+    sessionhelper.login("admin", "secret");
+    contactHelper = new ContactHelper(wd);
+  }
 
-    public void logout() {
-      wd.findElement(By.linkText("Logout")).click();
-    }
+  public void stop() {
+    wd.quit();
+  }
 
-    public void returnToGroupPage() {
-      wd.findElement(By.linkText("group page")).click();
-    }
 
-    public void submitGroupCreation() {
-      wd.findElement(By.name("submit")).click();
-    }
 
-    public void fillGroupForm(GroupData groupData) {
-      wd.findElement(By.name("group_name")).click();
-      wd.findElement(By.name("group_name")).clear();
-      wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-      wd.findElement(By.name("group_header")).click();
-      wd.findElement(By.name("group_header")).clear();
-      wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-      wd.findElement(By.name("group_footer")).click();
-      wd.findElement(By.name("group_footer")).clear();
-      wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
 
-    public void initGroupCreation() {
-      wd.findElement(By.name("new")).click();
-    }
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
 
-    public void gotoGroupPage() {
-      wd.findElement(By.linkText("groups")).click();
-    }
-
-    public void stop() {
-        wd.quit();
-    }
-
-    public boolean isElementPresent(By by) {
-      try {
-        wd.findElement(by);
-        return true;
-      } catch (NoSuchElementException e) {
-        return false;
-      }
-    }
-
-    public boolean isAlertPresent() {
-      try {
-        wd.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
-    }
-
-    public void deleteSelectedGroups() {
-      wd.findElement(By.name("delete")).click();
-    }
-
-    public void selectGroup() {
-      wd.findElement(By.name("selected[]")).click();
-    }
+  public ContactHelper getContactHelper() {
+    return contactHelper;
+  }
 }
